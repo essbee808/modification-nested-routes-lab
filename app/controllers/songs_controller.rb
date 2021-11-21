91b75cba6b2c81ddd295c3ef5a660d4f3c7eba9f
essbee808 <sheena.myb@gmail.com>
2021-11-20 23:22:10 -0800

    Create helper methods and modify form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,29 +26,35 @@ class SongsController < ApplicationController
 
   def new
     #binding.pry
-    @song = Song.new(artist_id: params[:artist_id])
+    #@song = Song.new(artist_id: params[:artist_id])
       
     #check if artist is real
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artists_path
     else
-      @song = Artist.new(artist_id: params[:artist_id])
+      @song = Song.new(artist_id: params[:artist_id])
     end
   end
 
   def create
-    binding.pry
+    #accepts and sets artist_id
     @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to artist_song_path(@artist, @song)
-    else
-      render :new
-    end
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist)
+      end
+    else 
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -73,7 +79,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
